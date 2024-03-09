e6a955e3b2dd0a5831a8a8178fda8b2916d29081
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-03-09 21:35:57 +0200

    added: content colections - cleaned up

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,35 @@
 import { defineCollection, z } from "astro:content";
-
+const work = defineCollection({
+  schema: z.object({
+    client: z.string().optional(),
+    description: z.string().optional(),
+    website: z.string().optional(),
+    thumbnail: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+const infopages = defineCollection({
+  schema: z.object({
+    page: z.string(),
+  }),
+});
 const postsCollection = defineCollection({
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-
-      tags: z.array(z.string())
-    })
- });
-
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
 export const collections = {
+  work: work,
+  infopages: infopages,
   posts: postsCollection,
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,134 +2,66 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
+    fontFamily: {
+      display: ["Boska", ...defaultTheme.fontFamily.sans],
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
+    },
     fontSize: {
-      xs: [
-        "0.75rem",
-        {
-          lineHeight: "1rem",
-        },
-      ],
-      sm: [
-        "0.875rem",
-        {
-          lineHeight: "1.5rem",
-        },
-      ],
-      base: [
-        "1rem",
-        {
-          lineHeight: "1.75rem",
-        },
-      ],
-      lg: [
-        "1.125rem",
-        {
-          lineHeight: "2rem",
-        },
-      ],
-      xl: [
-        "1.25rem",
-        {
-          lineHeight: "2rem",
-        },
-      ],
-      "2xl": [
-        "1.5rem",
-        {
-          lineHeight: "2rem",
-        },
-      ],
-      "3xl": [
-        "2rem",
-        {
-          lineHeight: "2.5rem",
-        },
-      ],
-      "4xl": [
-        "2.5rem",
-        {
-          lineHeight: "3.5rem",
-        },
-      ],
-      "5xl": [
-        "3rem",
-        {
-          lineHeight: "3.5rem",
-        },
-      ],
-      "6xl": [
-        "3.75rem",
-        {
-          lineHeight: "1",
-        },
-      ],
-      "7xl": [
-        "4.5rem",
-        {
-          lineHeight: "1.1",
-        },
-      ],
-      "8xl": [
-        "6rem",
-        {
-          lineHeight: "1",
-        },
-      ],
-      "9xl": [
-        "8rem",
-        {
-          lineHeight: "1",
-        },
-      ],
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.5rem" }],
+      base: ["1rem", { lineHeight: "1.75rem" }],
+      lg: ["1.125rem", { lineHeight: "2rem" }],
+      xl: ["1.25rem", { lineHeight: "2rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2rem" }],
+      "3xl": ["2rem", { lineHeight: "2.5rem" }],
+      "4xl": ["2.5rem", { lineHeight: "3.5rem" }],
+      "5xl": ["3rem", { lineHeight: "3.5rem" }],
+      "6xl": ["3.75rem", "1"],
+      "7xl": ["4.5rem", "1.1"],
+      "8xl": ["6rem", "1"],
+      "9xl": ["8rem", "1"],
     },
-    extend: {
-
-      colors: {
-
-        kotei: {
-        '50': 'hsl(207, 60%, 97%)',
-    '100': 'hsl(206, 50%, 94%)',
-    '200': 'hsl(204, 52%, 86%)',
-    '300': 'hsl(203, 53%, 74%)',
-    '400': 'hsl(202, 51%, 60%)',
-    '500': 'hsl(202, 49%, 48%)',
-    '600': 'hsl(204, 54%, 39%)',
-    '700': 'hsl(205, 54%, 32%)',
-    '800': 'hsl(204, 50%, 27%)',
-    '900': 'hsl(206, 44%, 24%)',
-    '950': 'hsl(207, 43%, 16%)',
-    '1000': 'hsl(215, 63%, 12%)',
-    '1050': 'hsl(210, 54%, 5%)',
-        },
-      },
-      fontFamily: {
-        display: ["Boska", ...defaultTheme.fontFamily.sans],
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
+    colors: {
+      neutral: colors.neutral,
+      white: colors.white,
+      transparent: colors.transparent,
+      kotei: {
+        50: "hsl(207, 60%, 97%)",
+        100: "hsl(206, 50%, 94%)",
+        200: "hsl(204, 52%, 86%)",
+        300: "hsl(203, 53%, 74%)",
+        400: "hsl(202, 51%, 60%)",
+        500: "hsl(202, 49%, 48%)",
+        600: "hsl(204, 54%, 39%)",
+        700: "hsl(205, 54%, 32%)",
+        800: "hsl(204, 50%, 27%)",
+        900: "hsl(206, 44%, 24%)",
+        950: "hsl(207, 43%, 16%)",
+        1000: "hsl(215, 63%, 12%)",
+        1050: "hsl(210, 54%, 5%)",
       },
     },
+
+    extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
+};
