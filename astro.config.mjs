8f63b72874dbb76b5e9bb565551ac5541e90853c
Martin <tejedam21@gmail.com>
2024-10-16 11:22:18 -0700

    Fix import issues and remove unused components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx"; 
+import path from 'path';
 
 export default defineConfig({
   markdown: {
@@ -19,5 +20,12 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), mdx()],
   content: {
     collections: ['work', 'posts', 'about']
-  }
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
